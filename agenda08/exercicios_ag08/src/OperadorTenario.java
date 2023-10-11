9d3d371a806f1e46e47b80140dfc61ce44837594
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-10-10 22:20:26 -0300

    Adicionando tabela verdade

--- agenda08/exercicios_ag08/src/OperadorTenario.java
+++ agenda08/exercicios_ag08/src/OperadorTenario.java
@@ -4,6 +4,21 @@ public class OperadorTenario {
         boolean taChovendo = false;
         boolean vamosAPraia = fimDeSemana && taChovendo;
 
+        // Tabela Verdade
+        // Operador && (AND)
+        /* true && true = true
+           true && false = false
+           false && true = false
+           false && false = false
+         */
+
+        /* Operador || (OR)
+            true || true = true
+            true || false = true
+            false || true = true
+            false || false= false
+         */
+
         System.out.println(vamosAPraia);
 
         String mensagem = fimDeSemana ? "É fim de semana" : "Não é fim de semana";
